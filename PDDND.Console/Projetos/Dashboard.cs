f5c6ba2400fcec8c3a62c80c74e1acdba03507fa
rafanoronha <rafanoronha@gmail.com>
2013-10-03 18:57:08 -0300

    exercise 1

---
+++ PDDND.Console/Projetos/Dashboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDND.Console.Projetos
+{
+    public class Dashboard
+    {
+        public Dashboard(IProjetos projetos)
+        {
+            Projetos = projetos;
+        }
+
+        public IProjetos Projetos { get; private set; }
+
+        public int TotalHorasTrabalhadas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Projeto> OrdenarProjetosPorTotalHoras()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Projeto> OrdenarProjetosPorTotalHorasDecrescente()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Projeto> FiltrarProjetosPorTotalHorasMinimo(int x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Projeto> FiltrarProjetosPorTotalHorasMaximo(int x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ PDDND.Console/Projetos/Projeto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDDND.Console.Projetos
+{
+    public class Projeto
+    {
+        public String Nome { get; set; }
+        public int HorasTrabalhadas { get; set; }
+    }
+
+}
